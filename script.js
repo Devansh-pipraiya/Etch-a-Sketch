704ff34e5fffdf6deabf6b795a1623bdde98648a
Devansh Linux <145137014+Devansh-pipraiya@users.noreply.github.com>
2026-02-26 20:08:19 +0530

    refactor: major code cleanup, event handling redesign, and new features
    - Add full UI with new control buttons, positioning, and styling
    - Implement working color presets for sketch pen
    - Add toggle-grid-line functionality
    - Add error handling in custom grid creation
    - Replace multiple event listeners with event delegation
    - Refactor JavaScript logic for more functionality and cleaner code

--- script.js
+++ script.js
@@ -1,46 +1,64 @@
 const container = document.querySelector("#container");
-const button = document.querySelectorAll("button");
+const buttonContainer = document.querySelector("#button-container");
+const clearGridButton = document.querySelector("#clear-grid");
+const toggleGridLine = document.getElementById("toggle-grid-line");
+
+let currentSketchColor = "#e8eaed ";
+
 
 function createGrid(num){
+    if (num === null || !(num) || typeof num !== 'number') return;
+
+    num = Math.min(num, 100);
     let boxwidth = 100 / num;
     num *= num;
-
+    container.replaceChildren();
+    
     for (let i=1; i<=num; i++ ){
         const div = document.createElement("div");
         div.classList.add("grid");
         div.style.width = `${boxwidth}%` ;
+        if (toggleGridLine.classList.contains("active")) {div.classList.add("grid-line")};
         container.appendChild(div);
     }
 }
-
-button[0].addEventListener("click", () => {
-    container.replaceChildren(); 
-    createGrid(prompt());
-});
-
-createGrid(3); // starting grid
+createGrid(7); // starting grid
 
 
 container.addEventListener("mouseover", (e)=>{
-
-    if (e.target.classList == "grid"){
-        e.target.classList.add("white");
+    if (e.target.classList.contains("grid")){
+        e.target.style.backgroundColor = currentSketchColor;
     }
-})
+});
 
 
 function clearGrid(){
     const grid = document.querySelectorAll("#container div");
-    grid.forEach(box => box.classList.remove("white"));
+    grid.forEach(box => box.style.backgroundColor="");
 }
+clearGridButton.addEventListener("click", clearGrid);
 
-button[1].addEventListener("click", clearGrid);
 
-button[2].addEventListener("click", (e)=>gridTemplate(e.target.dataset.grid));
-button[3].addEventListener("click", (e)=>gridTemplate(e.target.dataset.grid));
-button[4].addEventListener("click", (e)=>gridTemplate(e.target.dataset.grid));
-button[5].addEventListener("click", (e)=>gridTemplate(e.target.dataset.grid));
-function gridTemplate(target){
-    container.replaceChildren(); 
-    createGrid(target);
-}
+
+toggleGridLine.addEventListener("click", ()=>{
+    toggleGridLine.classList.toggle("active");
+    
+    const grid = document.querySelectorAll("#container div");
+    grid.forEach(box => box.classList.toggle("grid-line"));
+})
+
+
+buttonContainer.addEventListener("click", (e)=>{
+
+    if (e.target.dataset.type == "grid"){
+        createGrid(Number(e.target.dataset.grid));
+    }
+
+    if (e.target.dataset.type == "custom-grid"){
+        createGrid(+prompt("Enter box per sides: \n 100 is max"));
+    }
+
+    if (e.target.dataset.type == "color"){
+        currentSketchColor = e.target.dataset.color;
+    }
+});
